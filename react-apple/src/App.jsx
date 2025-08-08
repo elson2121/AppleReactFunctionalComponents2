65d8996e77ab0611c614e295790186213f870f9f
elson2121 <elsonsd21@gmail.com>
2025-08-08 17:49:09 -0300

    the day one of my reach apple website i convert to the react the html and the css , need the bootstrap more to work on still

---
+++ react-apple/src/App.jsx
@@ -0,0 +1,29 @@
+// import "./assets/css/bootstrap.css"
+import from "./cutom"
+import Header from './assets/components/Header/Header';
+ import Alertt from './assets/components/Alertt/Alertt';
+import FirstSection from './assets/components/FirstSection/FirstSection';
+import SecondSection from './assets/components/SecondSection/SecondSection';
+import ThirdSection from './assets/components/ThirdSection/ThirdSection';
+import FourthSection from './assets/components/FourthSection/FourthSection';
+import FifthSection from './assets/components/FifthSection/FifthSection';
+import SixthSection from './assets/components/SixthSection/SixthSection';
+import Footer from './assets/components/Footer/Footer';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Alertt/>
+      <FirstSection/>
+      <SecondSection/>
+      <ThirdSection/>
+      <FourthSection/>
+      <FifthSection/>
+      <SixthSection/>
+      <Footer/>
+    </>
+  );
+}
+
+export default App;
